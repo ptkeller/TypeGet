f7ca87e3e89de79c78288e5ddf2dd863d39e351f
ptkeller <p_keller@sbcglobal.net>
2015-09-23 13:52:34 -0700

    for testing

--- app/score/score.js
+++ app/score/score.js
@@ -1,5 +1,9 @@
-angular.module('typeget.score', [])
-	.controller('scoreCon', [ '$scope','$rootScope', function($scope, $rootScope){
+angular.module('typeget.score', [/*'firebase'*/])
+	.controller('scoreCon', [ '$scope','$rootScope', function($scope, $rootScope, /*$firebaseObject*/){
+		// var scoreFire = new Firebase('https://glaring-inferno-922.firebaseio.com/')
+
+		// var board = $firebaseObject(scoreFire);
+		// board.$bindTo($scope, 'data')
 		$scope.score = [{
 			score: $rootScope.playerScore
 		}]
