9dcf267cc8e1774b06cc6d3ed959f93f1d662aa5
ptkeller <p_keller@sbcglobal.net>
2015-09-23 17:31:34 -0700

    database made, not accessed

--- app/score/score.js
+++ app/score/score.js
@@ -1,11 +1,37 @@
-angular.module('typeget.score', [/*'firebase'*/])
-	.controller('scoreCon', [ '$scope','$rootScope', function($scope, $rootScope, /*$firebaseObject*/){
-		// var scoreFire = new Firebase('https://glaring-inferno-922.firebaseio.com/')
-
-		// var board = $firebaseObject(scoreFire);
-		// board.$bindTo($scope, 'data')
+angular.module('typeget.score', ['firebase'])
+	.controller('scoreCon', [ '$scope','$rootScope', '$firebase', function($scope, $rootScope, $firebase){
 		$scope.score = [{
 			score: $rootScope.playerScore
 		}]
-	}
-])
+
+		$scope.players = {
+			player: null
+		}
+		var ref = new Firebase('https://typeget.firebaseio.com/')
+		$scope.leadAdd = function(){
+			var playerRef = ref.child('playerBase');
+			playerRef.push({
+				player: $scope.players.player,
+				score: $rootScope.playerScore
+			})			
+		}
+		
+		
+		var playerBox = [];
+		ref.once("value", function(snapshot) {
+		  var playerDb = snapshot.val();
+		  for(var eachPlayer in playerDb){
+		  	var playStats = playerDb[eachPlayer];
+		  	for(var stat in playStats){
+		  		playerBox.push(playStats.stat)
+		  		for(var i = 0; i < playerBox.length; i++){
+		  			$('.testDiv').html(playerBox[i])
+		  		}
+		  	}
+		  }
+		}, function (errorObject) {
+  			console.log("The read failed: " + errorObject.code);
+		});
+
+
+}])
